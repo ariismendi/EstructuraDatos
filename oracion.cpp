fae5994ca7144dca559b17a73a47e91e990c0f5a
Mauricio Arismendi <m.arismendi02@ufromail.cl>
2020-11-29 20:48:31 -0300

    agregada la oracion

--- oracion.cpp
+++ oracion.cpp
@@ -2,9 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(){
 
+char sentence[40];
+scanf("%[^\n]s", sentence);
+
+for(int i = 0; i < sizeof(sentence); i++ ){
+   if(sentence[i] >=97 && sentence[i] <= 122){
+			sentence[i] = sentence[i] - 32;
+    }
+}
+
+
+printf("%s", sentence);
 
 
 return 0;

f152902af2adb2d3094fc71a0c6caf2635e8f0cc
Mauricio Arismendi <m.arismendi02@ufromail.cl>
2020-11-29 12:10:02 -0300

    acabado primo.cpp

--- primo.cpp
+++ primo.cpp
@@ -6,19 +6,34 @@
 
 int main(){
     int number;
+    int remainders = 0;
+
     printf("Escriba un numero entero para saber si es primo\n");
     scanf("%i", &number);
 
-    for(int i = 2; i < number; i++){
-        if (number%i == 0){
-        printf("fuckyou");
-        }
-        else {
-        printf("holi");
-        }
+    if(number == 1){
+        printf("Ni primo ni compuesto mi rey");
+
     }
 
+    else {
+    for(int i = 2; i < number; i++){
+        if (number % i == 0){
+            remainders ++;
+        }
 
+        else {
+            continue;
+        }
+
+    }
+        if (remainders == 0){
+            printf("Es primo");
+        }
+        else{
+            printf("No es primou");
+        }
+    }
 
     return 0;
 }
